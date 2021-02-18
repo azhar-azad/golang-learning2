f1965d1eb604ef55c49c17c519afb310eeab862b
azhar-azad <azhar.u.azad@gmail.com>
2021-02-18 23:31:36 +0600

    go-by-example added

---
+++ go-by-example/06_If_Else/if-else.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/*
+	Branching with if and else in Go is straight-forward.
+*/
+func main() {
+
+	// Here's a basic example.
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	// You can have an if statement without an else.
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	// A statement can precede conditionals; any variables
+	// declared in this statement are available in all branches.
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	/*
+		NOTES:
+		You don't need parentheses around contditions in Go,
+		but that the braces are required.
+
+		There is no ternary if in Go, so you'll need to use a full
+		if statement even for basic conditions.
+	*/
+}
+
+/*
+	7 is odd
+	8 is divisible by 4
+	9 has 1 digit
+*/
